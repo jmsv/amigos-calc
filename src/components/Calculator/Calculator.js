692aee31efadc853351b9d2af2d7e0edfd6492a6
James Vickery <dev@jamesvickery.net>
2019-10-13 02:27:01 +0100

    pluralisation and better styling

--- src/components/Calculator/Calculator.js
+++ src/components/Calculator/Calculator.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react'
+import pluralize from 'pluralize'
 
 import './Calculator.scss'
 import { Button } from '../Button/Button'
@@ -8,61 +9,67 @@ export const Calculator = () => {
 
   // TODO: Handle plurals
   const outputs = [
-    { name: 'Solar lights', scale: 0.1, dp: 0 },
-    { name: 'People reached', scale: 0.7, dp: 0 },
+    { name: 'Solar $NOUN', noun: 'light', scale: 0.1, dp: 0 },
+    { name: '$NOUN reached', noun: 'Person', scale: 0.7, dp: 0 },
     { name: 'Saved by families', scale: 7.2, dp: 2, currency: '$' },
-    { name: 'Extra hours of child study time', scale: 105, dp: 0 },
-    {
-      name: (
-        <span>
-          Tonnes of CO<sub>2</sub> emissions averted
-        </span>
-      ),
-      scale: 0.111,
-      dp: 2,
-    },
+    { name: 'Extra $NOUN of child study time', noun: 'hour', scale: 105, dp: 0 },
+    { name: '$NOUN of CO2 emissions averted', noun: 'Tonne', scale: 0.111, dp: 2 },
   ]
 
-  // TODO: Scale relative to number of solar panels
   const applyScale = ({ scale, dp, currency }) => {
     const offset = Math.pow(10, dp)
     const value = Math.floor(donation * scale * offset) / offset
     return currency ? value.toFixed(dp) : value
   }
 
+  const formatName = ({ name, noun }, value) => {
+    if (!noun) return name
+
+    return name.replace('$NOUN', pluralize(noun, value))
+  }
+
   return (
     <div className="calculator">
-      <div className="row center">
-        {/* TODO: Unit? */}
-        <div>A donation of</div>
+      <div className="row">
+        <div className="input-line">
+          <div>A donation of</div>
 
-        <div>
-          <input
-            title="Donation"
-            type="number"
-            min="1"
-            max="1000000"
-            step="1"
-            value={donation}
-            onChange={event => setDonation(event.target.value)}
-          />
+          <div className="currency">
+            <span>£</span>
+            <input
+              title="Donation"
+              pattern="^\$\d{1,3}(,\d{3})*(\.\d+)?$"
+              type="number"
+              min="1"
+              max="1000000"
+              step="1"
+              value={donation}
+              onChange={event => setDonation(event.target.value)}
+            />
+          </div>
+
+          <div>will result in:</div>
         </div>
 
-        <div>will result in:</div>
+        <div className="cta">
+          <a href={`https://www.amigos.org.uk/donate/fundraise/${donation}/credit-card`}>
+            {/* TODO: Yellow Donate button from Amigos site */}
+            <Button>Donate {donation}</Button>
+          </a>
+        </div>
       </div>
       <div className="outputs">
-        {outputs.map(output => (
-          <div key={output.name}>
-            {output.currency}
-            {applyScale(output)} {output.name}
-          </div>
-        ))}
-      </div>
+        {outputs.map(output => {
+          const value = applyScale(output)
+          const name = formatName(output, value)
 
-      <div class="cta">
-        <a href={`https://www.amigos.org.uk/donate/fundraise/${donation}/credit-card`}>
-          <Button>Donate {donation}</Button>
-        </a>
+          return (
+            <div key={name}>
+              {output.currency}
+              {value} {name}
+            </div>
+          )
+        })}
       </div>
     </div>
   )
